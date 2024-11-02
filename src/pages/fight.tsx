dee1a4c8cd30780caadf4281d131d2594d6bffc1
eduardonot <eduardo_not@hotmail.com>
2024-11-02 20:36:17 -0300

    feat: implements fight page

--- src/pages/fight.tsx
+++ src/pages/fight.tsx
@@ -1,3 +1,38 @@
+import { MockCharacter } from "@/mock/Character";
+import fighter from "@/mock/images/fighter.png";
+import villain from "@/mock/images/villain.png";
+import background from "@/mock/images/background.jpg";
+import { fight } from "@/rules/fight";
+
+interface CharacterAppearanceProps {
+  className?: string;
+}
+const CharacterAppearance = ({ className }: CharacterAppearanceProps) => {
+  return <img className={`h-[150px] ${className}`} src={fighter}></img>;
+};
+
+const OpponentAppearance = ({ className }: CharacterAppearanceProps) => {
+  return (
+    <img className={`h-[150px] -scale-x-100  ${className}`} src={villain}></img>
+  );
+};
+const Background = () => {
+  return <img className="h-max" src={background}></img>;
+};
+
 export const FightPage = () => {
-  return <div>Fight</div>;
+  const character = new MockCharacter();
+  const opponent = new MockCharacter();
+  fight(character, opponent);
+  return (
+    <main className="flex flex-col border p-4 items-center">
+      {/* <div>{JSON.stringify(character)}</div> */}
+
+      <div className="relative">
+        <Background />
+        <CharacterAppearance className="absolute bottom-[25%] left-[20%]" />
+        <OpponentAppearance className="absolute bottom-[25%] left-[80%]" />
+      </div>
+    </main>
+  );
 };
