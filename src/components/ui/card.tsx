560343dc087f38bb3ff7968744e977f1fc738974
eduardonot <eduardo_not@hotmail.com>
2024-12-13 07:38:14 -0300

    style: sets component color

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,20 +1,26 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { useTheme } from "../theme-provider";
 
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
-    {...props}
-  />
-));
+>(({ className, ...props }, ref) => {
+  const { theme } = useTheme();
+  const bgColor = theme === "dark" ? "bg-card/[.8]" : "bg-card/[.8]";
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        `rounded-lg border text-card-foreground shadow-sm ${bgColor}`,
+        className
+      )}
+      {...props}
+    />
+  );
+});
 Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<
