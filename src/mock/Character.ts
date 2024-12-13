1794c4bf0d769709cc9231956b2b01ccc3363a78
eduardonot <eduardo_not@hotmail.com>
2024-12-13 07:40:32 -0300

    chore: creates mock character

--- src/mock/Character.ts
+++ src/mock/Character.ts
@@ -11,13 +11,13 @@ export class MockCharacter implements Character {
 
   constructor() {
     this.name = faker.person.firstName();
-    this.level = faker.number.int({ min: 1, max: 5 });
-    this.experience = faker.number.float({ min: 0.0, max: 500.0 });
+    this.level = faker.number.int({ min: 1, max: 100 });
+    this.experience = faker.number.float({ min: 0.0, max: 50000.0 });
     this.attributes = {
-      constitution: faker.number.int({ min: 5, max: 10 }),
-      dexterity: faker.number.int({ min: 5, max: 10 }),
-      intelligence: faker.number.int({ min: 5, max: 10 }),
-      strength: faker.number.int({ min: 5, max: 10 }),
+      constitution: faker.number.int({ min: 5, max: 2000 }),
+      dexterity: faker.number.int({ min: 5, max: 2000 }),
+      intelligence: faker.number.int({ min: 5, max: 2000 }),
+      strength: faker.number.int({ min: 5, max: 2000 }),
     };
     this.isMage = faker.datatype.boolean();
   }
